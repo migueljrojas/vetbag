df2c415974b79f08cf845bd429c639fcf8cc5e79
Your Name <you@example.com>
2019-02-18 13:51:59 -0300

    updates

---
+++ src/_modules/product-slider/product-slider.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Constructor
+var ProductSlider = function() {
+  this.name = 'product-slider';
+  console.log('%s module', this.name.toLowerCase());
+};
+
+module.exports = ProductSlider;
